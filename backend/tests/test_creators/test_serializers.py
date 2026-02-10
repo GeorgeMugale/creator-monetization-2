bdf8dcf7e808faaa37f0c1918c4bdd9807e2a268
sepiso <mukelabaisepiso23@gmail.com>
2026-02-10 23:39:35 +0200

    feat: Add serializer  to update a creators profile
    - Add unittests
    - Updates:
    1. User informatiion
    2. User categories
    3. Wallet KYC information

--- backend/tests/test_creators/test_serializers.py
+++ backend/tests/test_creators/test_serializers.py
@@ -3,8 +3,146 @@ from tests.factories import UserFactory, CreatorCategoryFactory
 from apps.creators.serializers import (
     CreatorPublicSerializer,
     CreatorListSerializer,
-    CreatorCategorySerializer)
+    CreatorCategorySerializer,
+    UpdateCreatorProfileSerializer)
 from apps.creators.models import CreatorCategory
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@pytest.mark.django_db
+class TestUpdateCreatorProfileSerializer:
+
+    def test_serializer_update_creator_categories(self, user_factory):
+
+        c1 = CreatorCategoryFactory(name='cat1')
+        c2 = CreatorCategoryFactory(name='cat2')
+        CreatorCategoryFactory(name='cat3')
+        data = {
+            'category_slugs': [c1.slug, c2.slug]
+        }
+        profile = user_factory.creator_profile
+
+        serializer = UpdateCreatorProfileSerializer(
+            instance=profile, data=data, partial=True)
+        
+        assert serializer.is_valid()
+
+        updated_profile = serializer.save()
+        import pdb; pdb.set_trace; breakpoint()
+        assert updated_profile.categories.order_by('id').first().name == 'cat1'
+        assert updated_profile.categories.order_by('id').last().name == 'cat2'
+
+    def test_serializer_update_all_wallet_fields(self, user_factory):
+        from apps.wallets.models import WalletKYC
+        data = {
+            'wallet_kyc': {
+                "id_document_type": "NRC",
+                "id_document_number": "123456",
+                "account_type": "BANK",
+                "bank_name": "Standard Chartered",
+                "bank_account_name": "John Doe",
+                "bank_account_number": "1234567890",
+            }
+        }
+        profile = user_factory.creator_profile
+
+        serializer = UpdateCreatorProfileSerializer(
+            instance=profile, data=data, partial=True)
+        
+        assert serializer.is_valid()
+
+        updated_profile = serializer.save()
+        wallet = updated_profile.wallet
+
+        wallet_kyc = WalletKYC.objects.get(wallet=wallet)
+
+        assert wallet_kyc.bank_name == "Standard Chartered"
+        assert wallet_kyc.bank_account_name  =="John Doe"
+        assert wallet_kyc.bank_account_number == "1234567890"
+
+
+    def test_serializer_update_valid_fields(self, user_factory):
+        data = {
+            'first_name': 'test', 'last_name': 'surname', 'bio': '',
+            'phone_number': '', 'profile_image': None,
+            'cover_image': None, 'website': '', 'category_slugs': [], 'wallet_kyc': {}
+        }
+        profile = user_factory.creator_profile
+
+        serializer = UpdateCreatorProfileSerializer(
+            instance=profile, data=data, partial=True)
+        assert serializer.is_valid()
+        assert serializer.validated_data['first_name'] == "test"
+        assert serializer.validated_data['last_name'] == "surname"
+        assert serializer.errors == {}
+
+        updated_profile = serializer.save()
+        assert updated_profile.user.first_name == 'test'
+        assert updated_profile.user.last_name == 'surname'
+        assert User.objects.get(id=user_factory.id).first_name == 'test'
+
+    def test_serializer_update_valid_fields_with_missing_fields(self, user_factory):
+        data = {
+            'first_name': 'test', 'bio': 'My test bio',
+        }
+        profile = user_factory.creator_profile
+
+        serializer = UpdateCreatorProfileSerializer(
+            instance=profile, data=data, partial=True)
+        assert serializer.is_valid()
+        assert serializer.validated_data['bio'] == "My test bio"
+
+    def test_serializer_fails_update_invalid_fields(self, user_factory):
+        data = {
+            'first_name': 'test', 'category_slugs': '', 'wallet_kyc': ''
+        }
+        profile = user_factory.creator_profile
+
+        serializer = UpdateCreatorProfileSerializer(
+            instance=profile, data=data, partial=True)
+        assert not serializer.is_valid()
+        assert 'category_slugs' in serializer.errors
+        assert 'wallet_kyc' in serializer.errors
+
+    def test_serializer_update_image_fields(self, user_factory):
+        import io
+        from PIL import Image
+        from django.core.files.uploadedfile import SimpleUploadedFile
+
+        file_obj = io.BytesIO()
+        image = Image.new("RGBA", size=(1, 1), color=(255, 0, 0))
+        image.save(file_obj, 'png')
+        file_obj.seek(0)
+
+        fake_profile = SimpleUploadedFile(
+            name='test_profile.png',
+            content=file_obj.read(),
+            content_type='image/png'
+        )
+        file_obj.seek(0)
+        fake_cover = SimpleUploadedFile(
+            name='test_cover.png',
+            content=file_obj.read(),
+            content_type='image/png'
+        )
+        data = {
+            'profile_image': fake_profile, 'cover_image': fake_cover
+        }
+        profile = user_factory.creator_profile
+
+        serializer = UpdateCreatorProfileSerializer(
+            instance=profile, data=data, partial=True)
+        assert serializer.is_valid()
+
+        updated_profile = serializer.save()
+
+        assert updated_profile.profile_image is not None
+        assert updated_profile.cover_image is not None
+        assert updated_profile.profile_image.url is not None
+        assert updated_profile.cover_image.url is not None
+
 
 @pytest.mark.django_db
 class TestCreatoryCategorySerializer:
@@ -12,7 +150,7 @@ class TestCreatoryCategorySerializer:
         serializer = CreatorCategorySerializer(category_factory)
         expected_fields = {
             'name', 'slug', 'icon', 'is_featured', 'country_code', 'is_active'
-            }
+        }
         assert set(serializer.data.keys()) == expected_fields
 
 
